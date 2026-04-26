31e291bd5ec7441d66242f025e38945e59c68109
Aurorah Harmony <aurorahHarmony@gmail.com>
2026-04-26 15:16:14 +1200

    Update to deno 3

--- _config.ts
+++ _config.ts
@@ -1,41 +1,28 @@
 import lume from "lume/mod.ts";
 import favicon from "lume/plugins/favicon.ts";
-import jsx_preact from "lume/plugins/jsx_preact.ts";
+import jsx from "lume/plugins/jsx.ts";
 import tailwindcss from "lume/plugins/tailwindcss.ts";
 import postcss from "lume/plugins/postcss.ts";
-
-import tailWindOptions from "./tailwind.config.js";
 
 const site = lume({
   location: new URL("https://curious.toys"),
   src: "./src",
   server: {
-    page404: "/404/",
+    page404: "/404.html",
   },
 });
 
 site
   .ignore("README.md")
   .copy("static", ".")
-  .use(
-    favicon({
-      input: "/favicon.webp",
-    })
-  )
-  .use(jsx_preact())
-  .use(
-    tailwindcss({
-      extensions: [".mdx", ".jsx", ".tsx", ".md", ".html", ".njx"],
-      options: tailWindOptions,
-    })
-  );
-
-site.use(postcss());
+  .add("theme.css")
+  .use(favicon({ input: "/favicon.webp" }))
+  .use(jsx())
+  .use(tailwindcss())
+  .use(postcss());
 
 site.filter("currentYear", (): string => {
   return new Date().getFullYear().toString();
 });
-
-site.copy("static", ".");
 
 export default site;
